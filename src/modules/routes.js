90eb84a7834229c49067eaaeaadc076bb3006de5
MokhirbekOfficial <77888062+MokhirbekOfficial@users.noreply.github.com>
2021-12-21 15:58:17 +0500

    Progressing yet

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -6,18 +6,24 @@ const users = require('./users/users')
 const clinics = require('./clinics/clinics')
 const superAdmin = require('./superAdmin/superAdmin')
 const admin = require('./admin/admin')
+const token = require('./token/token')
 
 router
     .get('/', home.getClinics)
-    .post('/register', users.addusers)
-    .post('/login', users.checkuser)
-    .get('/services', clinics.getServices)
+    .get('/admin/orders', admin.getOrders)
     .get('/superadmin/users', superAdmin.getUsers)
     .get('/superadmin/orders', superAdmin.getOrders)
+
+    .post('/register', users.addusers)
+    .post('/login', users.checkuser)
+    .post('/tokenchecker', token.tokenchecker)
+    .post('/services', clinics.getServices)
+    .post('/superadmin/create/admin', users.addusers)
+    .post('/add/order', users.addOrder)
+
     .delete('/superadmin/order/delete', superAdmin.deleteOrder)
     .delete('/superadmin/user/delete', superAdmin.deleteUser)
     .delete('/superadmin/delete/clinics', superAdmin.deleteClinics)
-    .get('/admin/orders', admin.getOrders)
     .delete('/admin/order/delete', superAdmin.deleteOrder)
-    .post('superadmin/create/admin', users.addusers)
+    
 module.exports = router
